3d06883bedd464fbe9db958a68cdb9e2a17cec6d
Your Name <you@example.com>
2021-04-16 18:49:04 +0800

    all 4/16

---
+++ src/main/java/com/yxf/linkcommunity/LinkCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.yxf.linkcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yxf/linkcommunity/LinkCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yxf.linkcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
